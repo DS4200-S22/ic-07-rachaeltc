2acb0a4182f179ae3b4ee1f04113e52e5ef31ee9
Rachael <74115477+rachaeltc@users.noreply.github.com>
2022-02-28 13:22:30 -0500

    scatterplot finished

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -22,77 +22,71 @@ const svg3 = d3
   .attr("viewBox", [0, 0, width, height]);
 
 
-d3.csv("data/scatter.csv").then((data) => {
-  console.log(data); 
+d3.csv("data/scatter.csv").then((data3) => {
+  console.log(data3); 
+
   // find max score
-  let maxY = d3.max(data, function(d) { return d.score; });
-  
-  // sets y axis scale and range
-  let yScale = d3.scaleLinear()
-            .domain([0,maxY])
+let maxY2 = d3.max(data3, function(d) { return d.score; });
+
+let maxX2 = d3.max(data3, function(d) { return d.day; });
+
+// sets y axis scale and range
+let yScale2 = d3.scaleLinear()
+            .domain([0,maxY2])
             .range([height-margin.bottom,margin.top]); 
-            
-  // sets x axis scale and  range
-  let xScale = d3.scaleBand()
-            .domain(d3.range(data.length))
-            .range([margin.left, width - margin.right])
-            .padding(0.1); 
 
-  // places the y axis element on the svg  
-  svg3.append("g")
-    .attr("transform", `translate(${margin.left}, 0)`) 
-    .call(d3.axisLeft(yScale)) 
-    .attr("font-size", '20px'); 
+// Tsets x axis scale and  range
+let xScale2 = d3.scaleLinear()
+            .domain([0,maxX2])
+            .range([margin.left, width - margin.right]); 
 
-  // places the x axis element on the svg and adds tick labels
-  svg3.append("g")
+// places the y axis element on the svg  
+svg3.append("g")
+   .attr("transform", `translate(${margin.left}, 0)`) 
+   .call(d3.axisLeft(yScale2)) 
+   .attr("font-size", '20px'); 
+
+// places the x axis element on the svg and adds tick labels
+svg3.append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`) 
-    .call(d3.axisBottom(xScale) 
-            .tickFormat(i => data[i].day))  
+    .call(d3.axisBottom(xScale2))  
     .attr("font-size", '20px'); 
 
-  // sets up tooltip div to associate with csv scatter div
-  const tooltip3 = d3.select("#csv-scatter") 
-    .append("div") 
-    .attr('id', "tooltip3") 
-    .style("opacity", 0) 
-    .attr("class", "tooltip"); 
+// sets up tooltip div to associate with csv scatter div
+const tooltip2 = d3.select("#csv-scatter") 
+.append("div") 
+.attr('id', "tooltip2") 
+.style("opacity", 0) 
+.attr("class", "tooltip"); 
 
-  // in the case the mouse hovers over div, tooltip will unhide and display name and score 
-  const mouseover3 = function(event, d) {
-    tooltip3.html("Day: " + d.day + "<br> Score: " + d.score + "<br>") 
-    .style("opacity", 1);  
-  }
+// in the case the mouse hovers over div, tooltip will unhide and display day and score 
+const mouseover2 = function(event, d) {
+tooltip2.html("day: " + d.day + "<br> Score: " + d.score + "<br>") 
+.style("opacity", 1);  
+}
 
-  // tooltip is to be placed at specified positiosn relative to the mouse pointer
-  const mousemove3 = function(event, d) {
-    tooltip3.style("left", (event.x)+"px") 
-    .style("top", (event.y + yTooltipOffset) +"px"); 
-  }
+// tooltip is to be placed at specified positiosn relative to the mouse pointer
+const mousemove2 = function(event, d) {
+tooltip2.style("left", (event.x)+"px") 
+.style("top", (event.y + yTooltipOffset) +"px"); 
+}
 
-  // in the case the mouse leaves div, tooltip will hide again
-  const mouseleave3 = function(event, d) { 
-    tooltip3.style("opacity", 0); 
-  }
+// in the case the mouse leaves div, tooltip will hide again
+const mouseleave2 = function(event, d) { 
+tooltip2.style("opacity", 0); 
+}
 
   // adds rectangles according to data in data1 to divs with class bar, and has 
   // tooltip functionality 
-  svg3.selectAll("circle") 
-   .data(data) 
+  svg3.selectAll(".circ") 
+   .data(data3) 
    .enter()  
    .append("circle") 
-     .attr("cx", (d) => xScale(d.day)) 
-     .attr("cy", (d) => yScale(d.score)) 
-     .attr("r", 10) 
-     .on("mouseover", mouseover3) 
-     .on("mousemove", mousemove3)
-     .on("mouseleave", mouseleave3);
+     .attr("class", "circ") 
+     .attr("cx", (d) => xScale2(d.day)) 
+     .attr("cy", (d) => yScale2(d.score)) 
+     .attr("r", 10)
+     .on("mouseover", mouseover2) 
+     .on("mousemove", mousemove2)
+     .on("mouseleave", mouseleave2);
 });
-
-
-
-
-
-
-
-
